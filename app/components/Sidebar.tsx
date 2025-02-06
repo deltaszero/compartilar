0086dc7dfb5f4eb26a29fab3a4d63fff095e4927
dsthiago <thiagosd.2.18@gmail.com>
2025-02-06 19:39:12 -0300

    refactor Sidebar component; add support for subpages in navigation items and improve layout structure

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -10,8 +10,6 @@ import NavLink from '@components/ui/NavLink';
 import { useUser } from '@context/userContext';
 // importing assets
 import premiumImage from "@assets/images/hand_house_vertical_rect_2.jpg";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import TreeIcon from '@assets/icons/tree.svg';
 import LoginHeader from "@components/layout/LoginHeader";
 import IconMeuLar from '@assets/icons/icon_meu_lar.svg';
 import IconInfo from '@assets/icons/icon_meu_lar_info.svg';
@@ -25,8 +23,8 @@ import IconSettings from '@assets/icons/icon_meu_lar_settings.svg';
 // animation variants
 const sidebarVariants = {
     hidden: { x: -300, opacity: 0 },
-    visible: { 
-        x: 0, 
+    visible: {
+        x: 0,
         opacity: 1,
         transition: {
             type: "spring",
@@ -39,8 +37,8 @@ const sidebarVariants = {
 };
 const itemVariants = {
     hidden: { x: -20, opacity: 0 },
-    visible: { 
-        x: 0, 
+    visible: {
+        x: 0,
         opacity: 1,
         transition: {
             type: "spring",
@@ -55,54 +53,72 @@ interface NavItemProps {
     currentPath: string;
     icon: React.ReactNode;
     children: React.ReactNode;
+    subpages?: Array<{
+        path: string;
+        label: string;
+    }>;
 }
 
-const NavItem = ({ href, currentPath, icon, children }: NavItemProps) => {
-    const isActive = currentPath === href;
+const NavItem = ({ href, currentPath, icon, children, subpages }: NavItemProps) => {
+    const isActive = currentPath === href || subpages?.some(subpage => currentPath.startsWith(subpage.path));
+    const hasSubpages = subpages && subpages.length > 0;
 
     return (
-        <motion.div
-            variants={itemVariants}
-            whileHover={{ x: isActive ? 0 : 10 }}
-            className={`pl-2 flex flex-row gap-4 ${
-                isActive 
-                    ? 'text-xl font-bold text-primary'
-                    : 'font-light text-lg'
-            }`}
-        >
-            <div>
-                {icon}
-            </div>
-            <div className="flex flex-col justify-center">
+        <motion.div variants={itemVariants} whileHover={{ x: isActive ? 0 : 10 }} className={`${hasSubpages ? 'collapse collapse-arrow' : ''}`}>
+            <input 
+                type="radio" 
+                name="navigation-accordion"
+                className={`${hasSubpages ? '' : 'hidden'}`}
+            />
+            {/* Title Section */}
+            <div className="collapse-title flex flex-row min-h-0 gap-4">
+                <div>{icon}</div>
                 <NavLink href={href}>
-                    <p>{children}</p>
+                    <p className={`text-md hover:text-primary ${isActive ? 'text-primary font-bold text-lg' : ''}`}>
+                        {children}
+                    </p>
                 </NavLink>
             </div>
+
+            {/* Conditional Subpages Section */}
+            {hasSubpages ? (
+                <div className="collapse-content">
+                    {subpages.map(subpage => (
+                        <NavLink key={subpage.path} href={subpage.path}>
+                            <p className={`text-sm hover:text-primary ${currentPath === subpage.path ? 'text-primary text-lg' : ''}`}>
+                                {subpage.label}
+                            </p>
+                        </NavLink>
+                    ))}
+                </div>
+            ) : (
+                null
+            )}
         </motion.div>
     );
 };
 
 const PremiumCard = () => (
-<div className={`card card-compact shadow-xl mx-8 bg-white text-neutral`}>
-    <Image
-        src={premiumImage}
-        alt="Call to Action Image: Hand holding a house"
-        className='rounded-t-xl'
-    />
-    <div className="card-body flex flex-col gap-2">
-        <p className="card-title font-Raleway text-md">
-            Aprimore Sua Experiência
-        </p>
-        <p className="font-Raleway text-sm">
-            Consiga acesso a ferramentas avançadas para uma coparentalidade mais fluida e organizada.
-        </p>
-        <div className="card-actions justify-end">
-            <button className={`btn rounded-lg hover:border-primary bg-primary text-base-100 hover:bg-white hover:text-primary font-raleway`}>
-                Ver Planos
-            </button>
+    <div className={`card card-compact shadow-xl mx-8 bg-white text-neutral`}>
+        <Image
+            src={premiumImage}
+            alt="Call to Action Image: Hand holding a house"
+            className='rounded-t-xl'
+        />
+        <div className="card-body flex flex-col gap-2">
+            <p className="card-title font-Raleway text-md">
+                Aprimore Sua Experiência
+            </p>
+            <p className="font-Raleway text-sm">
+                Consiga acesso a ferramentas avançadas para uma coparentalidade mais fluida e organizada.
+            </p>
+            <div className="card-actions justify-end">
+                <button className={`btn rounded-lg hover:border-primary bg-primary text-base-100 hover:bg-white hover:text-primary font-raleway`}>
+                    Ver Planos
+                </button>
+            </div>
         </div>
     </div>
-</div>
 );
 
 export default function Sidebar() {
@@ -110,44 +126,67 @@ export default function Sidebar() {
     const pathname = usePathname();
 
     const navItems = userData ? [
-        { path: `/${userData.username}`,           label: 'Meu Lar',         icon: <IconMeuLar width={32} height={32} /> },
-        { path: `/${userData.username}/info`,      label: 'Informações',     icon: <IconInfo width={32} height={32} /> },
-        { path: `/${userData.username}/plan`,      label: 'Plano de Parentalidade', icon: <IconPlan width={32} height={32} /> },
-        { path: `/${userData.username}/calendar`,  label: 'Calendário',      icon: <IconCalendar width={32} height={32} /> },
-        { path: `/${userData.username}/finances`,  label: 'Finanças',       icon: <IconFinance width={32} height={32} /> },
-        { path: `/${userData.username}/handshake`, label: 'Decisões',      icon: <IconHandshake width={32} height={32} /> },
-        { path: `/${userData.username}/chat`,      label: 'Conversas',      icon: <IconChat width={32} height={32} /> },
-        { path: `/${userData.username}/settings`,  label: 'Configurações', icon: <IconSettings width={32} height={32} /> },
+        {
+            path: `/${userData.username}`, label: 'Meu Lar', icon: <IconMeuLar width={24} height={24} />,
+        },
+        { 
+            path: `/${userData.username}/info`, label: 'Informações', icon: <IconInfo width={24} height={24} /> ,
+            subpages: [
+                {
+                    path: `/${userData.username}/plan/resumo`,
+                    label: 'Resumo'
+                }
+            ]
+        },
+        {
+            path: `/${userData.username}/plan`, label: 'Plano de Parentalidade', icon: <IconPlan width={24} height={24} />,
+            subpages: [
+                {
+                    path: `/${userData.username}/plan/resumo`,
+                    label: 'Resumo'
+                }
+            ]
+        },
+        { path: `/${userData.username}/calendar`, label: 'Calendário', icon: <IconCalendar width={24} height={24} /> },
+        { path: `/${userData.username}/finances`, label: 'Finanças', icon: <IconFinance width={24} height={24} /> },
+        { path: `/${userData.username}/handshake`, label: 'Decisões', icon: <IconHandshake width={24} height={24} /> },
+        { path: `/${userData.username}/chat`, label: 'Conversas', icon: <IconChat width={24} height={24} /> },
+        { path: `/${userData.username}/settings`, label: 'Configurações', icon: <IconSettings width={24} height={24} /> },
     ] : [];
 
-    
 
     return (
-        <div className="sticky top-0 h-screen overflow-y-none flex flex-col justify-between pb-6 pt-3">
+        // <div className="flex flex-col justify-between sticky top-0 overflow-y-none pt-3">
+        <div className="flex flex-col justify-between h-screen overflow-y-none ">
             <div className={`text-neutral-content`}>
-                    <LoginHeader />
+                <LoginHeader />
                 <div className="divider mx-6"></div>
                 {/* menu */}
                 <motion.nav
                     initial="hidden"
                     animate="visible"
                     variants={sidebarVariants}
-                    className="flex flex-col gap-16"
-                > 
+                    className="flex flex-col gap-2"
+                >
                     {loading ? (
                         <motion.div variants={itemVariants} className="flex flex-col gap-4 mx-6 my-6">
                             <div className="skeleton h-8 w-full rounded-md"></div>
                             <div className="skeleton h-8 w-full rounded-md"></div>
+                            <div className="skeleton h-8 w-full rounded-md"></div>
+                            <div className="skeleton h-8 w-full rounded-md"></div>
+                            <div className="skeleton h-8 w-full rounded-md"></div>
+                            <div className="skeleton h-8 w-full rounded-md"></div>
                         </motion.div>
                     ) : (
-                        <motion.div className="flex flex-col gap-4 my-6" variants={itemVariants}>
+                        <motion.div className="flex flex-col" variants={itemVariants}>
                             {navItems.map(
                                 (item) => (
-                                    <NavItem 
+                                    <NavItem
                                         key={item.path}
                                         href={item.path}
                                         currentPath={pathname}
                                         icon={item.icon}
+                                        subpages={item.subpages}
                                     >
                                         {item.label}
                                     </NavItem>
@@ -162,5 +201,5 @@ export default function Sidebar() {
                 <PremiumCard />
             </div>
         </div>
-        );
+    );
 }
