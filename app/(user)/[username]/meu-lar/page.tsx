e75673ef081d139d54c91d8fc63124e4b5f101b4
dsthiago <thiagosd.2.18@gmail.com>
2025-02-12 10:50:03 -0300

    feat: add UserProfileBar component to SettingsPage; include new SVG icons for bottom navigation

--- app/(user)/[username]/meu-lar/page.tsx
+++ app/(user)/[username]/meu-lar/page.tsx
@@ -2,11 +2,13 @@
 'use client';
 
 import React from 'react';
+import UserProfileBar from "@components/UserProfileBar";
 
 export default function SettingsPage() {
 
     return (
         <div className="h-screen flex flex-col">
+            <UserProfileBar />
         </div>
     );
 }
--- app/(user)/[username]/page.tsx
+++ app/(user)/[username]/page.tsx
@@ -17,8 +17,8 @@ import LoadingPage from '@components/ui/LoadingPage';
 // import { SignupFormData } from '@/types/signup.types';
 // importing assets
 import CameraIcon from '@assets/icons/camera.svg';
-import IconMeuLar from '@assets/icons/icon_meu_lar.svg';
-import IconBell from '@assets/icons/icon_meu_lar_bell.svg';
+// import IconMeuLar from '@assets/icons/icon_meu_lar.svg';
+// import IconBell from '@assets/icons/icon_meu_lar_bell.svg';
 import { motion } from 'framer-motion';
 import UserProfileBar from "@components/UserProfileBar";
 // importring components 
@@ -41,27 +41,6 @@ export interface SignupFormData {
 //     gender: 'male' | 'female' | 'other' | null;
 //     relationship: 'biological' | 'adopted' | 'guardian' | null;
 // }
-
-
-const AvatarSection = ({ photoURL }: { photoURL?: string }) => (
-    <motion.div
-        className="mask mask-squircle w-16 h-16 bg-gray-100 flex items-center justify-center"
-        whileHover={{ scale: 1.1 }}
-    >
-        {photoURL ? (
-            <Image
-                src={photoURL}
-                width={128}
-                height={128}
-                alt="User avatar"
-                className="object-cover"
-                priority
-            />
-        ) : (
-            <CameraIcon className="w-12 h-12 text-gray-400" />
-        )}
-    </motion.div>
-);
 
 
 // const fetchChildren = async (parentId: string): Promise<KidInfo[]> => {
@@ -248,113 +227,121 @@ const UserNotFound = () => (
     </div>
 );
 
-
 const capitalizeFirstLetter = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
+const AvatarSection = ({ photoURL }: { photoURL?: string }) => (
+    <motion.div
+        className="mask mask-squircle bg-gray-100 flex items-center justify-center"
+        whileHover={{ scale: 1.1 }}
+    >
+        {photoURL ? (
+            <Image
+                src={photoURL}
+                width={128}
+                height={128}
+                alt="User avatar"
+                className="object-cover"
+                priority
+            />
+        ) : (
+            <CameraIcon className="w-12 h-12 text-gray-400" />
+        )}
+    </motion.div>
+);
 
-const UserNavbar = ({ userData }: { userData: Partial<SignupFormData> }) => {
-    const navItems = [
-        { path: `/${userData.username}`, label: 'Perfil', },
-        { path: `/${userData.username}/meu-lar`, label: 'Meu Lar', },
-        { path: `/${userData.username}/geolocation`, label: 'Localização',},
-        { path: `/${userData.username}/chat`, label: 'Chat',},
-        { path: 'mais', label: 'Mais',},
-    ];
+const UserProfileCard = ({ userData }: { userData: Partial<SignupFormData> }) => (
+    <div className="flex flex-col items-start w-full -mt-16 relative z-[9999] px-4 gap-1">
+        <AvatarSection photoURL={userData?.photoURL} />
+        <div className="flex flex-col gap-0">
+            <div className="text-2xl font-semibold">
+                {capitalizeFirstLetter(userData.firstName || '')} {capitalizeFirstLetter(userData.lastName || '')}
+            </div>
+            <div className="text-gray-500">
+                {userData.username}
+            </div>
+        </div>
+    </div>
+    // <div className="navbar bg-base-100">
+    //     <div className="flex-1">
+    //         <IconMeuLar width={32} height={32} />
+    //     </div>
+    //     <div className="flex-none gap-4">
+    //         <IconBell width={32} height={32} />
+    //         <div className="dropdown dropdown-end">
+    //             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+    //                 <AvatarSection photoURL={userData?.photoURL} />
+    //             </div>
+    //             <ul
+    //                 tabIndex={0}
+    //                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+    //                 <li>
+    //                     <a className="justify-between">
+    //                         Profile
+    //                         <span className="badge">New</span>
+    //                     </a>
+    //                 </li>
+    //                 <li><a>Settings</a></li>
+    //                 <li><a>Logout</a></li>
+    //             </ul>
+    //         </div>
+    //         <motion.div className="flex flex-col justify-center">
+    //             <div className="text-secondary-content">
+    //                 {capitalizeFirstLetter(userData.firstName || '')} {capitalizeFirstLetter(userData.lastName || '')}
+    //             </div>
+    //             <div className="text-gray-500">
+    //                 {userData.username}
+    //             </div>
+    //         </motion.div>
+    //     </div>
+    // </div>
+);
+
+
+export default function UserPage() {
+    const { username } = useParams();
+    const router = useRouter();
+    const { user, userData, loading } = useUser();
+    // check if user is logged in and if the user is the same as the one being accessed
+    useEffect(() => {
+        if (!loading) {
+            if (!user) router.push('/login');
+            else if (userData?.username !== username) router.push('/');
+        }
+    }, [user, userData, loading, username, router]);
+    // if (loading) return <LoadingSkeleton />;
+    const [initialLoading, setInitialLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setInitialLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading || initialLoading) return <LoadingPage />;
+    if (!userData) return <UserNotFound />;
 
     return (
-        <div className="flex flex-row items-center justify-between w-full p-4 bg-base-content text-white">
-            <p className="text-2xl">
-                {navItems.find(item => item.path === window.location.pathname)?.label}
-            </p>
-            <IconBell width={32} height={32} />
-        </div>
-    );
-}
-
-
-    const UserProfileCard = ({ userData }: { userData: Partial<SignupFormData> }) => (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <IconMeuLar width={32} height={32} />
-            </div>
-            <div className="flex-none gap-4">
-                <IconBell width={32} height={32} />
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <AvatarSection photoURL={userData?.photoURL} />
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
-                <motion.div className="flex flex-col justify-center">
-                    <div className="text-secondary-content">
-                        {capitalizeFirstLetter(userData.firstName || '')} {capitalizeFirstLetter(userData.lastName || '')}
-                    </div>
-                    <div className="text-gray-500">
-                        {userData.username}
-                    </div>
-                </motion.div>
-            </div>
-        </div>
-    );
-
-
-    export default function UserPage() {
-        const { username } = useParams();
-        const router = useRouter();
-        const { user, userData, loading } = useUser();
-
-        // check if user is logged in and if the user is the same as the one being accessed
-        useEffect(() => {
-            if (!loading) {
-                if (!user) router.push('/login');
-                else if (userData?.username !== username) router.push('/');
-            }
-        }, [user, userData, loading, username, router]);
-
-        // if (loading) return <LoadingSkeleton />;
-        const [initialLoading, setInitialLoading] = useState(true);
-
-        useEffect(() => {
-            const timer = setTimeout(() => {
-                setInitialLoading(false);
-            }, 3000);
-            return () => clearTimeout(timer);
-        }, []);
-
-        if (loading || initialLoading) return <LoadingPage />;
-        if (!userData) return <UserNotFound />;
-
-        return (
-            <div className="flex flex-col items-start overflow-hidden">
-                {/* NAVBAR */}
-                <UserProfileBar />
-                {/* BACKGROUND GRADIENT */}
-                <motion.div
-                    // className="h-48 bg-gradient-to-b from-base-content via-primary to-primary flex items-center justify-center w-full"
-                    className="
+        <div className="flex flex-col items-start overflow-hidden">
+            {/* NAVBAR */}
+            <UserProfileBar />
+            {/* BACKGROUND GRADIENT */}
+            <motion.div
+                // className="h-48 bg-gradient-to-b from-base-content via-primary to-primary flex items-center justify-center w-full"
+                className="
                     h-48 flex items-center justify-center w-full
-                    pattern-dots pattern-green-200 pattern-bg-white pattern-size-2 pattern-opacity-20
-                    "
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                >
-                </motion.div>
-                <div className="flex flex-row flex-start gap-8 w-full">
-                    {/* <section className="w-2/3 flex flex-col min-h-screen">
+                    pattern-wavy pattern-green-300 pattern-bg-white pattern-size-2 pattern-opacity-20
+                "
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+            </motion.div>
+            <UserProfileCard userData={userData} />
+            <div className="flex flex-row flex-start gap-8 w-full">
+                {/* <section className="w-2/3 flex flex-col min-h-screen">
                     {userData?.firstName ? (
                         <div className="flex flex-col items-left gap-4">
                             <UserProfile userData={{
@@ -369,12 +356,12 @@ const UserNavbar = ({ userData }: { userData: Partial<SignupFormData> }) => {
                         <UserNotFound />
                     )}
                 </section > */}
-                    {/* <section className="card w-1/3 bg-secondary p-4">
+                {/* <section className="card w-1/3 bg-secondary p-4">
                     <div className="flex flex-col items-center space-y-12">
                         <KidsGrid parentId={userData.uid} />
                     </div>
                 </section> */}
-                </div >
-            </div>
-        );
-    }
+            </div >
+        </div>
+    );
+}
